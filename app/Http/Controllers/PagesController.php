f9127d24a7b90b37eb40f824fc2b5e1157d8e3cb
Eduardo Murillo <edmz8802@gmail.com>
2017-07-03 15:55:06 -0700

    Base de datos y paginacion
    Se creo registros de base de datos con seed y paginacion de laravel

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,32 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
 
 class PagesController extends Controller
 {
     public function home(){
-        $messages = [
-            [
-                'id'=>1,
-                'content'=>'Este es mi primer mensaje',
-                'image'=>'http://lorempixel.com/600/338?1'
-            ],
-            [
-                'id'=>2,
-                'content'=>'Este es mi segundo mensaje',
-                'image'=>'http://lorempixel.com/600/338?2'
-            ],
-            [
-                'id'=>3,
-                'content'=>'Este es mi tercero mensaje',
-                'image'=>'http://lorempixel.com/600/338?3'
-            ],
-            [
-                'id'=>4,
-                'content'=>'Este es mi cuarto mensaje',
-                'image'=>'http://lorempixel.com/600/338?4'
-            ],
-        ];
+        $messages = Message::paginate(10);
 
         return view('welcome', [
             'messages' => $messages
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,17 +16,37 @@
     </div>
 
     <div class="row">
+        <form action="/messages/create" method="post">
+            <div class="form-group @if($errors->has('message')) has-danger @endif">
+                {{ csrf_field() }}
+                <input type="text" name="message" class="form-control" placeholder="Que estas pensando?">
+                @if ($errors->has('message'))
+                    @foreach ($errors->get('message') as $error)
+                        <div class="form-control-feedback">{{ $error }}</div>
+                    @endforeach
+                @endif
+            </div>
+        </form>
+    </div>
+
+    <div class="row">
         @forelse($messages as $message)
             <div class="col-6">
-                <img src="{{ $message['image'] }}" alt="img" class="thumbnail">
+                <img src="{{ $message->image }}" alt="img" class="thumbnail">
                 <p class="card-text">
                     {{ $message['content'] }}
-                    <a href="/messages/{{ $message['id'] }}">Leer mas</a>
+                    <a href="/messages/{{ $message->id }}">Leer mas</a>
                 </p>
             </div>
         @empty
             <p>No hay mensajes destacados.</p>
         @endforelse
+
+        @if(count($messages))
+        <div class="mt-2 mx-auto">
+            {{ $messages->links('pagination::bootstrap-4') }}
+        </div>
+        @endif
     </div>
 
 @endsection
